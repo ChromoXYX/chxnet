4c392a2ffc40b91a72e4832d35664153a2b898d5
ChromoXYX <thechromo@163.com>
2023-05-19 00:57:46 +0800

    start unit test, add pkgconfig

--- test/io_uring_acquire.cpp
+++ test/io_uring_acquire.cpp
@@ -1,3 +1,26 @@
-int main() {
-    return 1;
+#include "../include/chx/net.hpp"
+
+namespace net = chx::net;
+
+namespace chx::net::detail {
+namespace tags {
+struct test_acquire {};
+}  // namespace tags
+
+template <> struct async_operation<tags::test_acquire> {
+    void operator()(io_context* ctx) {
+        auto [sqe, task] = ctx->get();
+        io_uring_prep_nop(sqe);
+        task->__M_token.emplace([](auto...) -> int { return 0; });
+    }
+};
+}  // namespace chx::net::detail
+
+int main(void) {
+    net::io_context ctx;
+    for (int i = 0; i < 10000000; ++i) {
+        chx::net::detail::async_operation<
+            chx::net::detail::tags::test_acquire>()(&ctx);
+    }
+    return 0;
 }
