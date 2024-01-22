6d2507c7a0a064c6ea79ea875af02ee7ecc1121c
ChromoXYX <thechromo@163.com>
2024-01-22 20:31:36 +0800

    changed backstack() to boost.stacktrace

--- include/chx/net/exception.hpp
+++ include/chx/net/exception.hpp
@@ -4,11 +4,8 @@
 #include <string>
 #include <cstdint>
 #ifdef CHXNET_ENABLE_EXCEPTION_BACKTRACE
-#include <exception>
-#include <vector>
+#include <boost/stacktrace.hpp>
 #endif
-
-#define CHXNET_BACKTRACE_LEVEL 16
 
 namespace chx::net {
 /**
@@ -22,38 +19,21 @@ class exception : public std::exception {
     std::string __M_msg;
 
 #ifdef CHXNET_ENABLE_EXCEPTION_BACKTRACE
-    std::vector<std::string> __M_bt;
-#define __CHXNET_GENERATE_BT                                                   \
-    {                                                                          \
-        void* a[CHXNET_BACKTRACE_LEVEL];                                       \
-        int sz = ::backtrace(a, CHXNET_BACKTRACE_LEVEL);                       \
-        char** strs = ::backtrace_symbols(a, sz);                              \
-        try {                                                                  \
-            for (int i = 0; i < sz; ++i) {                                     \
-                __M_bt.emplace_back(strs[i]);                                  \
-            }                                                                  \
-        } catch (...) {                                                        \
-            ::free(strs);                                                      \
-            std::rethrow_exception(std::current_exception());                  \
-        }                                                                      \
-        ::free(strs);                                                          \
-    }
-#else
-#define __CHXNET_GENERATE_BT
+    boost::stacktrace::stacktrace __M_st;
 #endif
 
   public:
-    exception() { __CHXNET_GENERATE_BT; }
+    exception() {}
     exception(const exception&) = default;
     exception(exception&&) = default;
 
-    exception(const std::string& msg) : __M_msg(msg) { __CHXNET_GENERATE_BT; }
-    exception(const char* cmsg) : __M_msg(cmsg) { __CHXNET_GENERATE_BT; }
+    exception(const std::string& msg) : __M_msg(msg) {}
+    exception(const char* cmsg) : __M_msg(cmsg) {}
 
     const char* what() const noexcept(true) override { return __M_msg.c_str(); }
 #ifdef CHXNET_ENABLE_EXCEPTION_BACKTRACE
-    const std::vector<std::string>& backtrace() const noexcept(true) {
-        return __M_bt;
+    const boost::stacktrace::stacktrace& backtrace() const noexcept(true) {
+        return __M_st;
     }
 #endif
 };
